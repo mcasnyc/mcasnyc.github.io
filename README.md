68d3b882204ff0988c30af105e0c5380534f35b2
Quinn Leong <leongquinn@gmail.com>
2017-11-27 16:23:11 -0500

    add footer, make programs page switch on click, fix nav bar styling, add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+#### Credits
+
+- Facebook icon by [Dave Gandy](https://www.flaticon.com/authors/dave-gandy)
+- Instagram & email icons by [Cole Bemis](https://www.flaticon.com/authors/cole-bemis)
+- Twitter icon by [Elegant Themes](https://www.flaticon.com/authors/elegant-themes)
---
+++ assets/script.js
@@ -0,0 +1,257 @@
+var artOfTheActor = `
+<div>
+  <div class='description-title'>
+    Art of the Actor
+  </div>
+
+  <div class='description-section'>
+    <div class='description-header'>Profession actors's training in the Chekhov Technique</div>
+    <div>A 24 week Professional Actor's Training program in Michael
+    Chekhov Technique with Lenard Petit, and other Master
+    Teachers from the international Michael Chekhov Community.</div>
+  </div>
+
+  <div class='description-section'>
+    <div class='description-header'>12 Week Spring 2018 Intensive</div>
+    <div>February 19, to March 29, 2018 (6 week Technique Intensive & Clown)</div>
+    <div>April 1, to May 9, 2018 (6 week Rehearsal & Performance Skills)</div>
+  </div>
+
+  <div class='description-section'>
+    <div class='description-header'>12 Week Autumn 2018 Intensive</div>
+    <div>Mid September - Early December (Dates TBA)</div>
+    <div>Monday - Wednesday 10:00 am to 5:00 pm</div>
+    <div>Cost: $4,220.00 for each 12 weeks / or $2,110.00
+    for each 6 weeks (payment plans available)</div>
+    <div>A copy of The Michael Chekhov Handbook For the Actor by
+    Lenard Petit is complimentary in the cost of tuition.</div>
+  </div>
+</div>
+`;
+
+var workshops = `
+<div>
+  <div class='description-title'>
+    Workshops
+  </div>
+
+  <div class='description-section'>
+    <div class='description-header'>
+      Meet the Michael Chekhov Technique
+      <div>
+        January 18 to February 9, 2018
+      </div>
+    </div>
+    <div class='description-class-section'>
+      When you meet the Chekhov Technique you also meet your
+      own creative and expressive self. An introduction to the
+      working parts of the technique designed to energize and
+      provoke you, the actor, into playing fully in your body
+      and the life of the character. Playing, and improvisation
+      take place in a friendly and supportive space.
+    </div>
+    <div class='description-class-section'>
+      This workshop can be taken as an introduction, or as
+      a review.
+    </div>
+    <div class='description-class-section'>
+      <div>
+        Taught by Lenard Petit
+      </div>
+      <div>
+        4 weeks / 8 classes / Thursdays & Fridays /
+        10:00am-1:00pm / Cost: $390
+      </div>
+      <div>
+        $100 Deposit Payment with Registration
+      </div>
+    </div>
+  </div>
+
+  <div class='description-section'>
+    <div class='description-header'>
+      Acting The Song
+      <div>
+        February 15 to March 9, 2018
+      </div>
+    </div>
+    <div class='description-class-section'>
+      What is the story that lives inside the singer while singing
+      the song? How do you make contact with your inner life
+      so you can unlock and express the dramatic narrative
+      while performing the song?  The meaning is in the images
+      and the images can live in the body of the singer and
+      flow outward to the audience. The song is a creative event;
+      it’s meaning is supported by creative activity living in the
+      singer. Chekhov Technique is a direct pathway to the performer’s
+      expression. This technique workshop is specifically geared
+      towards the expression of the singer. It is an introduction
+      to the Michael Chekhov Technique for musical theatre actors.
+    </div>
+    <div class='description-class-section'>
+      No previous training in the Chekhov Technique is required.
+    </div>
+    <div class='description-class-section'>
+      <div>
+        Taught by Lenard Petit
+      </div>
+      <div>
+        4 weeks / 8 classes / Tuesdays & Wednesdays / 10:00am-1:00pm
+        / Cost: $390
+      </div>
+      <div>
+        $100 Deposit Payment with Registration
+      </div>
+    </div>
+  </div>
+</div>
+`;
+
+var chekhovPerformanceLab = `
+<div>
+  <div class='description-title'>
+    Chekhov Performance Lab
+  </div>
+
+  <div class='description-section'>
+    First Thursday of every month, actors with prior training in
+    Michael Chekhov are invited to bring material in to explore and
+    develop through the Technique.
+  </div>
+
+  <div class='description-section'>
+    A 3 hour session to support your exploration of performance.
+    Come with any prepared text or material, and choices to investigate.
+    Through experimentation with Chekhov's approach you will develop
+    your text with the group. This is a safe space to take Chekhov's
+    technique into your own hands. Previous workshops, intensive
+    training at MCAS and/ or study of the Michael Chekhov Technique
+    with a friend of the studio is required for eligibility to the
+    Chekhov Lab. Pay online or email to RSVP.
+  </div>
+
+  <div class='description-section'>
+    <div>Monthly: First Thursdays / 2:00 pm - 5:00 pm</div>
+    <div>Cost: $40 Single / $160 for Five</div>
+    <div>
+      Pay online
+      <a class='description-link' href='https://themichaelchekhovactingstudio.regfox.com/chekhov-performance-lab'>
+      here.</a>
+    </div>
+  </div>
+</div>
+`;
+
+var artistSalon = `
+<div>
+  <div class='description-title'>
+    Artist Salon
+  </div>
+
+  <div class='description-section'>
+    A series of intimate evenings presenting works-in-progress for
+    friends of the studio. Interested in presenting? Email a
+    description for consideration.
+  </div>
+
+  <div class='description-section'>
+    This impulse is to hold evenings for informal presentation,
+    discussion, or creation to appreciate art and its sustaining
+    power – a haven where artists can meet and share their creative
+    works. In the old sense of gathering and community, this is
+    the Artist’s Salon.
+  </div>
+
+  <div class='description-section'>
+    Next Salon event will be Saturday January 13, 2018!
+    <div><a class='description-link' href='info@michaelchekhvoactingstudio.com'>Email us </a>
+    if you are interested in being involved or help curate an event.</div>
+  </div>
+</div>
+`;
+
+var privateInstruction = `
+<div>
+  <div class='description-title'>
+    Private Instruction
+  </div>
+
+  <div class='description-section'>
+    Private coaching and small group study. Inquire for availability and cost.
+  </div>
+
+  <div class='description-section'>
+    PRIVATE COACHING for actors with experience in the technique.
+    PRIVATE GROUPS, introductory or continued instruction for groups of two or more artists.
+  </div>
+
+  <div class='description-section'>
+    <a class='description-link' href='info@michaelchekhvoactingstudio.com'>Email us</a> for availability and cost.
+  </div>
+</div>
+`;
+
+var mcasAtLotusBay = `
+<div>
+  <div class='description-title'>
+    MCAS at Lotus Bay
+  </div>
+
+  <div class='description-section'>
+    Actor’s Retreat Center for workshops and residencies at Lotus Bay on Lake Erie, NY.
+  </div>
+
+  <div class='description-section'>
+    A Summer Retreat Center for Actors on the shores of Lake Erie.
+    Lenard Petit and partner Meg Pantera bought the property in 2012,
+    taking on a house that was much neglected. They since have brought
+    it into a new life. Now a beautiful and restful space for artists
+    to retreat and create in the country. The first retreat was held
+    in August, 2017! Stay tuned for 2018 dates.
+  </div>
+</div>
+`;
+
+var beyondNewYork = `
+<div>
+  <div class='description-title'>
+    Beyond New York
+  </div>
+
+  <div class='description-section'>
+    Artistic Director Lenard Petit extends the Michael Chekhov Acting
+    Studio to workshops across the country and internationally.
+  </div>
+
+  <div class='description-section'>
+    <a class='description-link' href='info@michaelchekhvoactingstudio.com'>Contact us</a>
+    to invite Lenard to teach or direct at your University, School or Theater Company!
+  </div>
+</div>
+`;
+
+var DESCRIPTIONS = {
+  'ART OF THE ACTOR': artOfTheActor,
+  'WORKSHOPS': workshops,
+  'CHEKHOV PERFORMANCE LAB': chekhovPerformanceLab,
+  'ARTIST SALON': artistSalon,
+  'PRIVATE INSTRUCTION': privateInstruction,
+  'MCAS AT LOTUS BAY': mcasAtLotusBay,
+  'BEYOND NEW YORK': beyondNewYork
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  var descriptionBox = document.getElementsByClassName('program-description')[0];
+  var cards = Array.from(document.getElementsByClassName('program-card'));
+  cards.slice(1).forEach(c => {
+    c.addEventListener('click', function(e) {
+      var title;
+      if (e.target.classList[0] === 'program-card-title') {
+        title = e.target.innerText;
+      } else {
+        title = e.target.children[0].innerText;
+      }
+      descriptionBox.innerHTML = DESCRIPTIONS[title];
+    });
+  });
+});
