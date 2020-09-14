79851cd9c1d9bc94e7dc720e4560b7beffdd3655
mcasnyc <33729160+mcasnyc@users.noreply.github.com>
2020-09-14 16:50:35 -0400

    Update programs.js

--- assets/programs.js
+++ assets/programs.js
@@ -3,11 +3,12 @@ var artOfTheActor = `
 
   <div class='description-title'>
     Art of the Actor (On Pause) Please see our online programs. Here:
-
+  </div>
+  <div class='description-title'>
     By The Book Beyond The Book -- Online Study of Michael Chekhov's Book, To The Actor facilitated by Emma Wade
     Fridays 11:00am-12:30pm Eastern Time (US & Canada). Starting October 2020. 
 
-<a class='register-button' target='_blank' href='https://https://themichaelchekhovactingstudio.regfox.com/to-the-actor'>
+  <a class='register-button' target='_blank' href='https://https://themichaelchekhovactingstudio.regfox.com/to-the-actor'>
     Sign Up Today
   </a>
 
