1b7ea56e9b434a54657e4b1f860d99011b4ab2d4
mcasnyc <33729160+mcasnyc@users.noreply.github.com>
2022-04-27 13:06:35 -0400

    Update programs.js
    Date update

--- assets/programs.js
+++ assets/programs.js
@@ -5,9 +5,18 @@ var artOfTheActor = `
   </div>
 
  <div class='description-section'>
-  Psychological Gesture with Lenard Petit -- SIX Tuesdays 11:00am-1:00pm Eastern Time (US & Canada), May 3-June 7. Space Limited.
+  Architecture of Psychological Gesture with Lenard Petit -- SIX Tuesdays 11:00am-1:00pm Eastern Time (US & Canada), May 3-June 7. Space Limited.
  </div>
   <a class='register-button' target='_blank' href='https://themichaelchekhovactingstudio.regfox.com/architecture-of-pgspring22'>
+    Sign Up Today
+  </a>
+  
+  
+  <div class='description-section'>
+   By The Book Beyond The Book -- Guided Reading of Michael Chekhov's Book, To The Actor -- facilitated by Emma Wade --
+   Next Session is Chapter 10: How To Approch The Part & Chapter 11: Concluding Notes = Meets over 5 weeks - May 21st & June 18th, 2022. See Sign-up link for more info.
+</div>
+  <a class='register-button' target='_blank' href='https://themichaelchekhovactingstudio.regfox.com/to-the-actor'>
     Sign Up Today
   </a>
   
@@ -18,13 +27,6 @@ var artOfTheActor = `
     Sign Up Today
   </a>
 
-  <div class='description-section'>
-   By The Book Beyond The Book -- Guided Reading of Michael Chekhov's Book, To The Actor -- facilitated by Emma Wade --
-   Next Session is Chapter 9: Diffrent Types of Perfomance = Meets 2 weeks - March 26th & April 2 2022. See Sign-up link for more info.
-</div>
-  <a class='register-button' target='_blank' href='https://themichaelchekhovactingstudio.regfox.com/to-the-actor'>
-    Sign Up Today
-  </a>
 
 
 <div class='description-title'>
